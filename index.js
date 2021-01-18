191b0325fca61fffa8a0d731e753e0077396950e
Sergio <santossergio759@gmail.com>
2021-01-18 13:37:05 -0300

    arquivos alterados

--- index.js
+++ index.js
@@ -3,6 +3,11 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const port = process.env.PORT || 3000;
+
+// EJS //
+
+app.set("view engine", "ejs");
 
 // Public //
 
@@ -11,20 +16,19 @@ app.use(express.static(path.join(__dirname + "/public")));
 // Routes //
 
 app.get("/", function(req, res){
-    res.sendFile(__dirname + "/content/index.html")
+    res.render("index")
 });
 
 app.get("/termos-de-uso", function(req, res){
-    res.sendFile(__dirname + "/content/termos-de-uso.html")
+    res.render("termos-de-uso")
 });
 
 app.get("/politica-de-privacidade", function(req, res){
-    res.sendFile(__dirname + "/content/politica-de-privacidade.html")
+    res.render("politica-de-privacidade")
 });
 
 // Servidor //
 
-const port = process.env.PORT || 3000;
 app.listen(port, function () {
     console.log('Umbler listening on port %s', port);
 });
